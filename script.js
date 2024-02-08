ddf902f1a27fea95382acf10bff8157e8af465ea
hhitchcock24 <157539475+hhitchcock24@users.noreply.github.com>
2024-02-07 23:43:04 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,78 @@
 
+    require([
+      "esri/WebScene",
+      "esri/views/SceneView",
+      "esri/Camera",
+      "esri/widgets/Home",
+      "dojo/domReady!"
+    ], function(WebScene, SceneView, Camera, Home) {
+
+    
+      /*var map = new Map({
+        basemap: "streets",
+        ground: "world-elevation"
+      });*/
+      var scene = new WebScene({
+        portalItem:{
+         id:"0f6dc0310134482c9ec4acc1ac22ffe3" 
+        }
+      });
+      
+      var camera0 = new Camera({
+        position: [
+          -90.1994, // lon
+          38.627, // lat
+          50000// elevation in meters
+        ],
+        tilt:0,
+        heading: 0
+      })
+      
+      var camera = new Camera({
+        position: [
+          -90.1952, // lon
+          38.6526, // lat
+          5000000// elevation in meters
+        ],
+        tilt:0,
+        heading: 0
+      })
+      
+
+      var view = new SceneView({
+        container: "viewDiv",
+        map: scene,
+        viewingMode:"global",
+        camera: camera0,
+        environment: {
+            lighting: {
+              date: new Date(),
+              directShadowsEnabled: true,
+              // don't update the view time when user pans.
+              // The clock widget drives the time
+              cameraTrackingEnabled: false
+            }
+        },
+    });
+    
+    var homeBtn = new Home({
+        view: view
+      });
+
+      // Add the home button to the top left corner of the view
+    view.ui.add(homeBtn, "top-left");
+    
+    [mos].forEach(function(button) {
+      button.style.display = 'flex';
+      view.ui.add(button, 'top-right');
+    });
+    
+    mos.addEventListener('click', function() {
+      // reuse the default camera position already established in the homeBtn
+      view.goTo({
+        target:camera
+      });
+    });
+
+
+    });
